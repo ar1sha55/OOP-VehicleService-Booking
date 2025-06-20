739d3746f9ebc83d3489a4a0948761cfd4f1d781
Nur Arisha <147675471+ar1sha55@users.noreply.github.com>
2025-06-21 05:09:26 +0800

    CleaningBooking (inherit Booking)

---
+++ CleaningBooking.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+class CleaningBooking extends Booking {
+    private static int cID = 2000;
+    private CleaningPackage selectedPkg;
+
+    public CleaningBooking(Customer customer, Vehicle vehicle, LocalDate date, LocalTime time, CleaningPackage selectedPkg) {
+        super(cID++, customer, vehicle, date, time);
+        this.selectedPkg = selectedPkg;
+    }
+
+    @Override
+    public void printDetails() {
+        System.out.println("== CLEANING BOOKING ==");
+        System.out.println("Booking ID: " + cID);
+        System.out.println("Customer Name: " + customer.getName());
+        System.out.println("Vehicle Reg No: " + customer.getName()); //tunggu vehicle class siap
+        System.out.println("Package Name: " + selectedPkg.getName());
+        System.out.println("Package Description: " + selectedPkg.getDescription());
+        System.out.println("Package Price: " + selectedPkg.getPrice());
+        System.out.println("Booking Date: " + bookingDate);
+        System.out.println("Booking Time: " + bookingTime);
+        System.out.println("Status: " + status);   
+    }
+}
