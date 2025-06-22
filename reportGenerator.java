624c0627afd54cf70b0e61a4d31118ed61bdcaec
Ain Najiha <148193874+ainjhaa@users.noreply.github.com>
2025-06-22 11:45:47 +0800

    reportGenerator.java (base)

---
+++ reportGenerator.java
@@ -0,0 +1,18 @@
+import java.io.*;
+//import java.time.LocalDate;
+
+public class reportGenerator {
+
+
+    public void generate(){
+        System.out.println("\n===== SERVICE BOOKING REPORT =====");
+        System.out.println("________________________________________________________________________________________________________________________");
+        
+        System.out.printf("|%-3s|%-25s|%-20s|%-14s|%-14s|%-13s|%-13s|%-10s|\n",
+                         "ID", "Customer Name", "Service Type", "Vehicle Type", "Vehicle Plate", "Booking Date", "Booking Time", "Status");
+        System.out.println("------------------------------------------------------------------------------------------------------------------------");
+        /*for (Vehicle v : vehicle) {
+            
+        }*/
+    }
+}
