a2058d299f80b5bbc8384e979fc4ca41ba5ff329
Ain Najiha <148193874+ainjhaa@users.noreply.github.com>
2025-06-23 21:58:34 +0800

    Update reportGenerator.java

--- reportGenerator.java
+++ reportGenerator.java
@@ -14,28 +14,25 @@ public class ReportGenerator {
 
     public void generate(){
         System.out.println("\n===== CUSTOMER VEHICLES REPORT =====");
-        System.out.println("________________________________________________________________________________________________________________________");
+        System.out.println("+-------------------------------------------------------------------------------------------------------------------------------+");
         
-        System.out.printf("| %-3s|%-25s|%-10s|%-10s|%-10s|%-13s|%-13s|%-10s |\n",
+        System.out.printf("| %-3s| %-24s| %-14s| %-14s| %-14s| %-14s| %-14s| %-14s |\n",
                          "No", "Customer Name", "Vehicle Model", "Vehicle Type", "Vehicle Plate", "Vehicle Brand", "Vehicle Model", "Vehicle Color");
-        System.out.println("------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("+----+-------------------------+---------------+---------------+---------------+---------------+---------------+----------------+");
         
         try{
-            if(customers.isEmpty()){
-                System.out.println("No customer in database.");
+            int i=1;
+            for (Customer v : customers) {
+                System.out.printf("| %-3d %-25s %-15s %-15s %-15s %-15s %-15s %-15s |\n", 
+                                    i, v.getName(), v.getVehicle().getModel(), v.getVehicle().getVehicleType(), v.getVehicle().getPlateNum(), v.getVehicle().getBrand(), v.getVehicle().getModel(), v.getVehicle().getColour());
+                i++;
             }
-            else{
-                int i=1;
-                for (Customer v : customers) {
-                    System.out.printf("| %-3d %-25s %-10s %-10s %-10s %-13s %-13s %-10s |\n", 
-                                                i, v.getName(), v.getVehicle().getModel(), v.getVehicle().getVehicleType(), v.getVehicle().getPlateNum(), v.getVehicle().getBrand(), v.getVehicle().getModel(), v.getVehicle().getColour());
-                }
-            }
+            System.out.println("+----+-------------------------+---------------+---------------+---------------+---------------+---------------+----------------+\n");
         }
         catch (NullPointerException e) {
             System.out.println("Error: Missing data in customer or vehicle records.");
         } catch (Exception e) {
-            System.out.println("Unexpected error occurred while generating report: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
         
     }
