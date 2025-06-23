7cce6417cbd3405da5a22ed3a4648c63ab9cd932
Aziidan Ng <147889344+AziidanNg@users.noreply.github.com>
2025-06-23 21:52:07 +0800

    Update CleaningPackage.java

--- CleaningPackage.java
+++ CleaningPackage.java
@@ -1,28 +1,72 @@
 public enum CleaningPackage {
-    BASIC("Basic Wash", "Exterior wash only", 20.00),
-    DELUXE("Deluxe Wash", "Exterior + Interior vacuum", 35.00),
-    PREMIUM("Premium Detail", "Full detailing, wax, tire shine", 70.00);
+    BASIC("Basic Wash", "Exterior wash only", 7.00, 12.00, 20.00, 15, 20, 25),
+    DELUXE("Deluxe Wash", "Exterior + Interior vacuum", 15.00, 20.00, 30.00, 25, 30, 45),
+    PREMIUM("Premium Detail", "Full detailing, wax, tire shine", 70.00, 100.00, 150.00, 60, 80, 100);
 
     private final String name;
     private final String description;
-    private final double price;
+    private final double priceSedan, priceSuv, priceMpv;
+    private final int durationSedan, durationSuv, durationMpv;
 
-    CleaningPackage(String name, String description, double price) {
+    CleaningPackage(String name, String description, double priceSedan, double priceSuv, double priceMpv, int durationSedan, int durationSuv, int durationMpv) {
         this.name = name;
         this.description = description;
-        this.price = price;
+        this.priceSedan = priceSedan;
+        this.priceSuv = priceSuv;
+        this.priceMpv = priceMpv;
+        this.durationSedan = durationSedan;
+        this.durationSuv = durationSuv;
+        this.durationMpv = durationMpv;
     }
 
     public String getName() { return name; }
     public String getDescription() { return description; }
-    public double getPrice() { return price; }
+    /*
+    public double getPriceSedan() { return priceSedan; }
+    public double getPriceSuv() { return priceSuv; }
+    public double getPriceMpv() { return priceMpv; }
+    public int getDurationSedan() { return durationSedan; }
+    public int getDurationSuv() { return durationSuv; }
+    public int getDurationMpv() { return durationMpv; }
+    */
+
+    //Based on vehicle type
+    public double getPrice(VehicleType type) { 
+        switch(type) {
+            case SEDAN: return priceSedan;
+            case SUV: return priceSuv;
+            case MPV: return priceMpv;
+            default: throw new IllegalArgumentException("Invalid type");
+        }
+    }
+    public double getDuration(VehicleType type) { 
+        switch(type) {
+            case SEDAN: return durationSedan;
+            case SUV: return durationSuv;
+            case MPV: return durationMpv;
+            default: throw new IllegalArgumentException("Invalid type");
+        }
+    }
 
     public static void showAllPackages() {
         System.out.println("Available Cleaning Packages:");
         int index = 1;
         for (CleaningPackage p : CleaningPackage.values()) {
-            System.out.println(index++ + ". " + p.name + " - RM" + p.price);
-            System.out.println("   " + p.description);
+            System.out.println(index++ + ". " + p.getName());
+            System.out.println("   " + p.getDescription());
+            System.out.println("   Prices:");
+            System.out.println("     SEDAN - RM" + p.getPrice(VehicleType.SEDAN) + " (" + p.getDuration(VehicleType.SEDAN) + " mins)");
+            System.out.println("     SUV   - RM" + p.getPrice(VehicleType.SUV)   + " (" + p.getDuration(VehicleType.SUV)   + " mins)");
+            System.out.println("     MPV   - RM" + p.getPrice(VehicleType.MPV)   + " (" + p.getDuration(VehicleType.MPV)   + " mins)");
+        }
+    }
+    public static void showSuggestedPackages(VehicleType type) {
+        System.out.println("Suggested Cleaning Packages based on your Vehicle:");
+        int index = 1;
+        for (CleaningPackage p : CleaningPackage.values()) {
+            System.out.println(index++ + ". " + p.getName());
+            System.out.println("   " + p.getDescription());
+            System.out.println("   Prices: RM" + p.getPrice(type) + " (" + p.getDuration(type) + " mins)");
         }
     }
 
