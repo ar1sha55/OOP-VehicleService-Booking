a7cb93fbe33ef85087ef6966367012f949286cfd
Aziidan Ng <147889344+AziidanNg@users.noreply.github.com>
2025-06-25 20:16:32 +0800

    Update Service.java

--- Service.java
+++ Service.java
@@ -43,22 +43,22 @@ public class Service {
     //Display service info with different price and duration based on vehicle type
     public void displayInfo(VehicleType type){
         System.out.println("[" + id + "]" + name);
-        System.out.println("    Description" + description);
+        System.out.println("    Description: " + description);
 
         switch (type) {
             case SEDAN:
-            System.out.printf("    Price: RM%.2f\n" + sedanPrice);
-            System.out.println("    Duration: " + sedanDuration + "minutes");
+            System.out.printf("    Price: RM%.2f\n", sedanPrice);
+            System.out.println("    Duration: " + sedanDuration + " minutes");
             break;
 
             case SUV:
-            System.out.printf("    Price: RM%.2f\n" + suvPrice);
-            System.out.println("    Duration: " + suvDuration + "minutes");
+            System.out.printf("    Price: RM%.2f\n", suvPrice);
+            System.out.println("    Duration: " + suvDuration + " minutes");
             break;
 
         case MPV:
-            System.out.printf("    Price: RM%.2f\n" + mpvPrice);
-            System.out.println("    Duration: " + mpvDuration + "minutes");
+            System.out.printf("    Price: RM%.2f\n", mpvPrice);
+            System.out.println("    Duration: " + mpvDuration + " minutes");
             break;
         }
 
